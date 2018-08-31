7dec2f3727fad4f570b45cf706360dd019b0eb56
kadaokaduan <manhongpeng2711@163.com>
2018-08-31 10:19:35 +0800

    test

---
+++ crypt.c
@@ -0,0 +1,6 @@
+/* crypt.c (dummy version) -- do not perform encryption
+ * Hardly worth copyrighting :-)
+ */
+#ifdef RCSID
+static char rcsid[] = "$Id: //depot/mainline/cmd/gzip-1.2.4/crypt.c#1 $";
+#endif
